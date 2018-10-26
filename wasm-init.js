a7be8ff700b43be58eb028c19ccb9fcf77b47dc5
Charles Reusnow <Charles.J.Reusnow@centene.com>
2018-10-26 12:05:48 -0500

    Testing some debugging for mobile.

--- wasm-init.js
+++ wasm-init.js
@@ -1,38 +1,33 @@
-try {
-    const init = (go, wasmFileLocation) => {
-        return new Promise(
-            (resolve, reject) => {
-                console.log("Initializing Web Assembly...");
-                console.log(JSON.stringify(window.WebAssembly));
-                if ("WebAssembly" in window) {
-                    console.log(JSON.stringify(window.WebAssembly));
-                    window.WebAssembly.instantiateStreaming(fetch(wasmFileLocation), go.importObject).then(
-                        (instanceDetails) => {
-                            console.log("Success: Web Assembly initialized.");
-                            console.log("Running Web Assembly...");
-                            go.run(instanceDetails.instance).then(
-                                () => {
-                                    console.log("Success: Web Assembly ran.");
-                                    resolve();
-                                },
-                                reject
-                            );
-                        },
-                        reject
-                    );
-                } else {
-                    reject("This browser does not support Web Assembly!");
-                }
+const init = (go, wasmFileLocation) => {
+    return new Promise(
+        (resolve, reject) => {
+            console.log("Initializing Web Assembly...");
+            if ("WebAssembly" in window) {
+                console.log(JSON.stringify(Object.getOwnPropertyNames(window.WebAssembly)));
+                window.WebAssembly.instantiateStreaming(fetch(wasmFileLocation), go.importObject).then(
+                    (instanceDetails) => {
+                        console.log("Success: Web Assembly initialized.");
+                        console.log("Running Web Assembly...");
+                        go.run(instanceDetails.instance).then(
+                            () => {
+                                console.log("Success: Web Assembly ran.");
+                                resolve();
+                            },
+                            reject
+                        );
+                    },
+                    reject
+                );
+            } else {
+                reject("This browser does not support Web Assembly!");
             }
-        );
-    };
-
-    init(new Go(), "bin/out.wasm").catch(
-        (error) => {
-            console.error("Error: Web Assembly not initialized:");
-            console.error(error);
         }
     );
-} catch (e) {
-    alert(e);
-}
+};
+
+init(new Go(), "bin/out.wasm").catch(
+    (error) => {
+        console.error("Error: Web Assembly not initialized:");
+        console.error(error);
+    }
+);
